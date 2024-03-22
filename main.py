bbd13e228b308a53eeb6ed220dbc4cf04614231a
thiagobordini <thiagobordini@outlook.com>
2024-03-22 11:26:54 -0300

    Projeto Base

---
+++ main.py
@@ -0,0 +1,12 @@
+#Sistema de calculadora com valores digitados pelo usuário
+print("MENU CALCULADORA")
+print("")
+print("1 - Soma")
+print("2 - Subtração")
+print("3 - Multiplicação")
+print("4 -")
+print("")
+
+tipo_conta = input("Digite a opção desejável de cálculo: ")
+print("")
+
